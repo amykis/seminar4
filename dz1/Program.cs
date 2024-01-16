ba2f6556bae78107f554ffbd7024c13fc7ca52ed
Aleksey Myakinin <amykis@mail.ru>
2024-01-16 15:36:16 +0300

    add function GetArrayLength

---
+++ dz1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 1: Напишите программу, которая бесконечно запрашивает целые числа с консоли.
+// Программа  завершается при вводе символа `q` или при вводе числа, сумма цифр которого четная.
+
+int GetArrayLength(int number)
+{
+    int count = 0;
+    while (number > 0)
+    {
+        number /= 10;
+        count++;
+    }
+    return count;
+}
+
+// int GetSumOfNumbers(int number)
+// {
+//     for (number > 0)
+// }
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(GetArrayLength(number));
