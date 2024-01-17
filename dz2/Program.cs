af1b44d1b9dfcac734c1509b236ea8818a7ae6db
Aleksey Myakinin <amykis@mail.ru>
2024-01-17 13:19:48 +0300

    add dz2

---
+++ dz2/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача №2: Задайте массив заполненный случайными трёхзначными числами.
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// Пример [344 452 341 125] => 2
+
+Console.Clear();
+
+// Функция создания массива заполненный трёхзначными числами
+
+int[] CreateRandomArrayThreeDigitNumbers(int min, int max)
+{
+    int[] array = new int[3];
+    for (int i = 0; i < 3; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+    }
+    return array;
+}
+
+// Функция подсчёта чётных чисел в массиве
+
+void FindCountOfEvenNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            count++;
+        }
+
+    }
+    Console.WriteLine(count);
+}
+
+int min = 100;
+int max = 999;
+
+int[] array = CreateRandomArrayThreeDigitNumbers(min, max);
+
+FindCountOfEvenNumbers(array);
+
+// // Проверка своего кода Для вывода массива в консоль убрать "//" в следующих 5 строках
+
+//     for ( int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write($"{array[i]} ");
+//     }
+
+//     Console.WriteLine();
