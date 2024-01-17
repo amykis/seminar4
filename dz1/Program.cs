474a82dc2ec25cb6fb1385f91d6e8008b3920176
Aleksey Myakinin <amykis@mail.ru>
2024-01-17 11:01:59 +0300

    add branch secondOption

--- dz1/Program.cs
+++ dz1/Program.cs
@@ -1,8 +1,6 @@
 ﻿// Задача 1: Напишите программу, которая бесконечно запрашивает целые числа с консоли.
 // Программа  завершается при вводе символа `q` или при вводе числа, сумма цифр которого четная.
 
-Console.Clear();
-
 // Функция определения длины массива
 
 int GetArrayLength(int number)
@@ -28,29 +26,57 @@ int[] FillArray(int size, int number)
     return array;
 }
 
-// int GetSumOfNumbers(int sum)
-// {
-//     for (number > 0)
-// }
+// Функция определяет четное число или нечетное
 
+bool GetSumOfLastTwoElement(int[] array, int size)
+{
+    int sum = array[size - 1] + array[size - 2];
+    if (sum % 2 == 0)
+    {
+        return true;
+    } 
+    else 
+    {
+       return false;
+    } 
+}
 
-// Ввод числа и отправка его в функцию GetArrayLength()
+bool sum = false;
+string word = "";
+ 
+while (sum == false || word == "q")
+{
+    Console.Clear();
 
-Console.WriteLine("Введите число: ");
-int number = Convert.ToInt32(Console.ReadLine());
-int size = GetArrayLength(number);
+    // Ввод числа и отправка его в функцию GetArrayLength()
 
-// Создание массива из введенного числа, вызов функции FillArray()
+    Console.WriteLine("Введите число: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    word = Convert.ToString(number);
+    int size = GetArrayLength(number);
 
-int[] array = FillArray(size, number);
+    // Создание массива из введенного числа, вызов функции FillArray()
 
-// Для проверки длины массива убрать "//" в следующей строке
+    int[] array = FillArray(size, number);
 
-// Console.WriteLine(GetArrayLength(number));
+    // Для проверки длины массива убрать "//" в следующей строке
 
-// Для вывода массива в консоль убрать "//" в следующих 4 строках
+    // Console.WriteLine(GetArrayLength(number));
 
-// for ( int i = 0; i < array.Length; i++)
-// {
-//     Console.Write($"{array[i]} ");
-// }
+    // Для вывода массива в консоль убрать "//" в следующих 5 строках
+
+    // for ( int i = 0; i < array.Length; i++)
+    // {
+    //     Console.Write($"{array[i]} ");
+    // }
+
+    // Console.WriteLine();
+
+    // Проверка bool
+
+    // Console.WriteLine(GetSumOfLastTwoElement(array, size));
+
+    sum = GetSumOfLastTwoElement(array, size);
+}
+
+Console.WriteLine("[STOP]");
